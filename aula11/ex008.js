d9aca82a34dd2d5e2cce8ca59ea32a3655366a95
Melissa Martins <melmartins.pro@gmail.com>
2023-12-26 11:25:12 -0300

    criado o ex009 e ex 010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade dp seu carro é ${vel} km/h`)
+if (vel > 60){//condiçao simples!
+    console.log(`Você ultrapassou a velocidade permitida,vc esta MULTADO`)
+}
+console.log(`Drija sempre usando cinto de segurança!`)
