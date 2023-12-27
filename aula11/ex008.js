8554dd35fad7b246e054ef4b4f85c15f4c17c341
Melissa Martins <melmartins.pro@gmail.com>
2023-12-27 14:14:44 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 78.2
-console.log(`A velocidade dp seu carro é ${vel} km/h`)
-if (vel > 60){//condiçao simples!
-    console.log(`Você ultrapassou a velocidade permitida,vc esta MULTADO`)
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log(`Drija sempre usando cinto de segurança!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
