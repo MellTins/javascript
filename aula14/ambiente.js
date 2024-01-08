008925ffbbc964da8d286d3241125566706b7560
Melissa Martins <melmartins.pro@gmail.com>
2024-01-08 15:36:21 -0300

    aula sobre for

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/* console.log('Vai começar...')
+for(var c=1; c<=4;c++){
+    console.log(c)
+}
+console.log('Fim') */
+
+/* for(var c =1;c <= 10;c++){
+  console.log(`Você comeu ${c}º pedaço`)
+} */
+for(var contador = 1; contador <= 10;contador++){
+   console.log(`essa é a minha ${contador} vez fazendo esse exercicio`)
+}
