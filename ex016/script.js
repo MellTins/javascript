d9ee0e98b4b1c13e04afe7de481dd87487cd447b
Melissa Martins <melmartins.pro@gmail.com>
2024-01-09 13:55:21 -0300

    refiz o exercicio seguindo a correçao

--- ex016/script.js
+++ ex016/script.js
@@ -4,16 +4,33 @@
         var res = window.document.getElementById('res')
        
     function Salto(){
-      var valorn1 = Number(n1.value) //pega o value de n1
-      var valorfim = Number(fim.value) //pega o value de fim
       if(valorn1 == 0 || valorfim == 0 || passo==0){
         res.innerHTML = 'Impossivel Contar!'
       }else{
-        while(valorn1 <= valorfim){
-          res.innerHTML += `${valorn1} 👉🏼`
-          valorn1 += Number(passo.value) // aqui ele vai adicionando o numero de salto ate o numero determinado na var "valorfim"
+       res.innerHTML = "Contando... <br>"
+       var valorn1 = Number(n1.value) //pega o value de n1
+       var valorfim = Number(fim.value) //pega o value de fim
+       var p = Number(passo.value)
+       if (p <= 0){
+        window.alert('Passo invalido! Considerando Passo1')
+         p = 1
+       }
+
+        if(valorn1 < valorfim)
+        {for (let c = valorn1; c <= valorfim; c+= p) {
+          res.innerHTML += `${c} \u{1F449}` // aqui ele vai adicionando o numero de salto ate o numero determinado na var "valorfim"
+          //contagem crescente
         }
+        
+      }else {
+        //contagem regressiva
+           for (let c = valorn1; c >= valorfim; c-=p) {
+            res.innerHTML += `${c} \u{1F449}`
+           }
+        }
+         res.innerHTML += `\u{1F38C}`
       }
-    }
+      }
+    
           
         
--- ex017/script.js
+++ ex017/script.js
@@ -1,1 +1,25 @@
-alert('Olá')
+var ntxt = window.document.querySelector('input#ntxt')
+var res = window.document.getElementById('res')
+
+
+function tabuada(){
+    var valor = Number(ntxt.value)
+    var c = 0
+    for (var c = 1; c<= 10; c++) {
+        var resultado = valor *c
+        res.innerHTML += `${valor} x ${c} :${resultado} ` 
+        
+    }
+}
+
+//var ntxt = window.document.querySelector('input#ntxt')
+var res = window.document.getElementById('res')
+function tabuada(){
+    var valor = Number(ntxt.value)
+    var c = 0
+    var igual = valor * Number(c)
+    while(c <= 10){
+      res.innerHTML += `O resultado entre ${valor}e ${c} é: ${igual} `
+      valor += c
+    }
+}
